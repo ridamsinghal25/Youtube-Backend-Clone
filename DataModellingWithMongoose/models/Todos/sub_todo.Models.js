d4c490fbe79b11e46fe8cda7f7207bf92213aaf4
Ridam-Singhal <manusinghal84@gmail.com>
2023-11-27 07:33:17 +0530

    Added Todo and subTodo schema

--- DataModellingWithMongoose/models/Todos/sub_todo.Models.js
+++ DataModellingWithMongoose/models/Todos/sub_todo.Models.js
@@ -1,5 +1,21 @@
 import mongoose from "mongoose";
 
-const subTodoSchema = new mongoose.Schema({}, { timestamps: true });
+const subTodoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
 export const subTodo = mongoose.model("SubTodo", subTodoSchema);
--- DataModellingWithMongoose/models/Todos/todo.Models.js
+++ DataModellingWithMongoose/models/Todos/todo.Models.js
@@ -1,5 +1,28 @@
 import mongoose from "mongoose";
 
-const todoSchema = new mongoose.Schema({}, { timestamps: true });
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    subTodos: [
+      // type: Array
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubTodo",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export const Todo = mongoose.model("Todo", todoSchema);
