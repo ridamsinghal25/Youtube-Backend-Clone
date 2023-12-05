0631b1ce19ba6c272ec130ad4c191d3773734ba8
Ridam-Singhal <manusinghal84@gmail.com>
2023-12-05 14:28:57 +0530

    creating custom api

--- MegaProject/src/index.js
+++ MegaProject/src/index.js
@@ -1,13 +1,30 @@
 // require("dotenv").config({ path: "./env" });
 
+// import app from app.js
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    // ASSIGNMENT
+    app.on("error", (err) => {
+      console.log("ERROR : ", err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed !!! ", err);
+    process.exit(1);
+  });
 
 /*
 import mongoose from "mongoose";
@@ -15,7 +32,8 @@ import { DB_NAME } from "./constants";
 
 
 import express from "express";
-const app = express()(async () => {
+const app = express()
+(async () => {
   try {
     await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
 
---
+++ MegaProject/src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (requstHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requstHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => async () => {}
+// const asyncHandler = (func) => {async () => {}}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
