221f4719e6054be0517bf0c801d4e23bbf970241
Ridam-Singhal <manusinghal84@gmail.com>
2023-12-07 13:33:31 +0530

    writing register controller

--- MegaProject/src/controllers/user.Controller.js
+++ MegaProject/src/controllers/user.Controller.js
@@ -1,9 +1,49 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.Model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  return res.status(200).json({
-    message: "chai aur code",
+  /* Steps for user Register */
+  //  Data collection from user
+  //  Validation - not empty
+  //  check if user already exists: username , email
+  //  check for images, check for avatar
+  //  upload them to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return response
+
+  const { fullName, email, username, password } = req.body;
+  console.log("email: ", email);
+
+  // if (fullName === "") {
+  //   throw new ApiError(400, "fullName is required")
+  // }
+
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  // console.log(avatarLocalPath);
+
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is reqired");
+  }
 });
 
 export { registerUser };
