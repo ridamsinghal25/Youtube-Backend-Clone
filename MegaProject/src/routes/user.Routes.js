c87944d86d5282b260c48a8bb032cf46093334ac
Ridam-Singhal <manusinghal84@gmail.com>
2023-12-29 20:52:03 +0530

    add new routes for user controller

--- MegaProject/src/routes/user.Routes.js
+++ MegaProject/src/routes/user.Routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeUserPassword,
+  getCurrentUserDetails,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateUserAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.Controller.js";
 import { upload } from "../middlewares/multer.Middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -29,5 +36,21 @@ router.route("/login").post(loginUser);
 // secured
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeUserPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUserDetails);
+
+router.route("/update-account").patch(verifyJWT, updateUserAccountDetails);
+
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/update-coverImage")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
