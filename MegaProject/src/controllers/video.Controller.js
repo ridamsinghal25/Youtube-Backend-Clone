18f66cbd4e7395e08f84b9b867d4d391cd9ea460
Ridam-Singhal <manusinghal84@gmail.com>
2024-01-03 14:58:56 +0530

    Writing video controller

---
+++ MegaProject/src/controllers/video.Controller.js
@@ -0,0 +1,8 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const uploadVideo = asyncHandler((req, res) => {
+  // steps to upload video
+  // take video from system
+  // validate it - not empty
+  // uplooad to cloudinary
+});
