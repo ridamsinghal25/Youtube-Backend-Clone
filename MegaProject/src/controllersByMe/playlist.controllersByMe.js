66294383a2266b83f6c1d153697e027f72737bfd
Ridam-Singhal <manusinghal84@gmail.com>
2024-01-08 18:12:05 +0530

    added controller to get all user playlist and to get playlist by id

--- MegaProject/src/controllersByMe/playlist.controllersByMe.js
+++ MegaProject/src/controllersByMe/playlist.controllersByMe.js
@@ -21,6 +21,10 @@ const createPlaylist = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
+  if (!videoId) {
+    throw new ApiError(400, "video id is required");
+  }
+
   if (!isValidObjectId(videoId) || !isValidObjectId(userId)) {
     throw new ApiError(400, "Invalid id");
   }
@@ -41,4 +45,64 @@ const createPlaylist = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, playlist, "Playlist created successfully"));
 });
 
-export { createPlaylist };
+const getUserPlaylists = asyncHandler(async (req, res) => {
+  // Steps to get all user playlists
+  // take userId from req.params
+  // validate userId
+  // find user in playlist database to get all playlist created by user
+  // validate it
+  // response
+
+  const { userId } = req.params;
+
+  if (!userId) {
+    throw new ApiError(400, "user id is required");
+  }
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid user id");
+  }
+
+  const userPlaylists = await Playlist.find({ owner: userId });
+
+  if (!userPlaylists) {
+    throw new ApiError(400, "playlists not found");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, userPlaylists, "user playlists fetched successfully")
+    );
+});
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+  // Steps to get playlist by id
+  // take playlist id from req.params
+  // validate it
+  // use findById() method to find playlist
+  // validate it
+  // response
+
+  const { playlistId } = req.params;
+
+  if (!playlistId) {
+    throw new ApiError(400, "playlist id is required");
+  }
+
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid playlist id");
+  }
+
+  const playlist = await Playlist.findById(playlistId);
+
+  if (!playlist) {
+    throw new ApiError(404, "playlist not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "playlist fetched successfully"));
+});
+
+export { createPlaylist, getUserPlaylists, getPlaylistById };
--- MegaProject/src/routesByMe/playlist.routesByMe.js
+++ MegaProject/src/routesByMe/playlist.routesByMe.js
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createPlaylist } from "../controllersByMe/playlist.controllersByMe.js";
+import {
+  createPlaylist,
+  getPlaylistById,
+  getUserPlaylists,
+} from "../controllersByMe/playlist.controllersByMe.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
 router.route("/:videoId").post(createPlaylist);
+
+router.route("/user/:userId").get(getUserPlaylists);
+
+router.route("/:playlistId").get(getPlaylistById);
 
 export default router;
